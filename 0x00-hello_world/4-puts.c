a0ce41e4a442ab9d46150364ea879d4d7e1838a9
salllma <salma.radwan489@gmail.com>
2023-06-15 23:47:22 +0200

    C program that prints a phrase

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of a program to print a phrase 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
