d6895045e42e03f9077ca2e1326393b2d7f79f2e
salllma <salma.radwan489@gmail.com>
2023-06-21 04:13:16 +0200

    0. _putchar Task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+
+/*prototypes*/
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
