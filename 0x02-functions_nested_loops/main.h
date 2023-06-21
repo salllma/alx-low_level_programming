cdd46b449b100e6c4dfca2f54d177525d6629240
salllma <salma.radwan489@gmail.com>
2023-06-22 01:57:14 +0200

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,12 @@ int _putchar(char c);
 int print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
-
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
 
 #endif
