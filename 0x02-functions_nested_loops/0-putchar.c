1957fcce29391a6f1a19790a91ff63119488381b
salllma <salma.radwan489@gmail.com>
2023-06-21 03:54:52 +0200

    Pour l'amour de Betty <3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - entry point to a program that print 
- * a _putchar string 
- * 
+ * main - entry point to a program that print
+ * a _putchar string
+ *
  * Return: always 0 on success
 */
 
 int main(void)
 {
-	printf("_putchar");
+	printf("_putchar\n");
 
-	return 0;
+	return (0);
 }
